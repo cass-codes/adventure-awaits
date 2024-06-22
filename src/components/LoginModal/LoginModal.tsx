17f716af09a163aaec8e0c3a01b59b1f2bf440b2
Cass Barnard <30302915+cass-codes@users.noreply.github.com>
2024-06-21 21:17:36 -0400

    Login Modal

---
+++ src/components/LoginModal/LoginModal.tsx
@@ -0,0 +1,41 @@
+import Modal from "../basics/Modal";
+import "./LoginModal.css";
+
+function LoginModal({
+  isOpen,
+  onClose,
+}: {
+  isOpen: boolean;
+  onClose: Function;
+}) {
+  function onCloseHandler() {
+    onClose();
+  }
+
+  function loginHandler(data: any) {
+    data.preventDefault();
+    console.log("login", data);
+  }
+
+  function signUpHandler(data: any) {
+    console.log("signUp", data);
+  }
+
+  return (
+    <Modal hasCloseBtn={true} isOpen={isOpen} onClose={onCloseHandler}>
+      <form className="LoginModal" onSubmit={loginHandler}>
+        <h2>Login / Sign Up</h2>
+        <input type="text" placeholder="Username" required />
+        <input type="password" placeholder="Password" required />
+        <div className="LoginButtonRow">
+          <button type="submit">Login</button>
+          <button type="button" onClick={signUpHandler}>
+            Sign Up
+          </button>
+        </div>
+      </form>
+    </Modal>
+  );
+}
+
+export default LoginModal;
